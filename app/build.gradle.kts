8ed9c55a1d76ed080c85482aea194025478c58a4
Josportal <jose23lm@gmail.com>
2025-02-24 10:56:24 +0100

    Añadidos paquetes data y domain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,50 +1,45 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.jose.lopez.cryptobalance"
-    compileSdk = 33
+    namespace = "Jose.Lopez.cryptobalance"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.jose.lopez.cryptobalance"
-        minSdk = 21
-        targetSdk = 33
+        applicationId = "Jose.Lopez.cryptobalance"
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
     }
-
     buildFeatures {
         compose = true
-        viewBinding = true
     }
-
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -53,22 +48,22 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation("androidx.compose.ui:ui:1.4.7")
-    implementation("androidx.compose.material:material:1.4.7")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.7")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.7")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.7")
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    // Dependencias de Room
+    implementation("androidx.room:room-runtime:2.5.2") // Reemplaza con la última versión disponible
+    kapt("androidx.room:room-compiler:2.5.2") // Reemplaza con la última versión disponible
+    implementation("androidx.room:room-ktx:2.5.2") // Reemplaza con la última versión disponible
 }
